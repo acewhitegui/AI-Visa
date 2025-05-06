f028a8669ca9f0750fcb51d0ed27a63578e5814e
guipeng <aceguipeng@gmail.com>
2025-05-06 13:06:33 +0800

    update build script

--- cms/scripts/build.sh
+++ cms/scripts/build.sh
@@ -5,7 +5,7 @@ ls
 
 # 定义变量
 REGISTRY="registry.ap-southeast-1.aliyuncs.com/whitedit"
-IMAGE_NAME="any-converters-cms"
+IMAGE_NAME="ai-visa-cms"
 DOCKER_VERSION="${DOKCER_VERSION:-latest}"
 
 docker build --build-arg STRAPI_API_TOKEN="$STRAPI_API_TOKEN" --build-arg NEXT_PUBLIC_STRAPI_API_URL="$NEXT_PUBLIC_STRAPI_API_URL" --build-arg STRAPI_API_URL="$STRAPI_API_URL" -t ${REGISTRY}/${IMAGE_NAME}:"${DOCKER_VERSION}" . -f Dockerfile
--- cms/scripts/upgrade.sh
+++ cms/scripts/upgrade.sh
@@ -1,3 +1,3 @@
-DOCKER_IMAGE="registry.ap-southeast-1.aliyuncs.com/whitedit/any-converters-cms"
+DOCKER_IMAGE="registry.ap-southeast-1.aliyuncs.com/whitedit/ai-visa-cms"
 docker pull ${DOCKER_IMAGE}
-docker service update any-converters_cms --image ${DOCKER_IMAGE} --force
+docker service update ai-visa_cms --image ${DOCKER_IMAGE} --force
