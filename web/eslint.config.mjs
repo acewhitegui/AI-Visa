089f936a59860abd15ce876651860d33a3e371c2
guipeng <aceguipeng@gmail.com>
2025-05-06 11:12:12 +0800

    update packages

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,6 +1,8 @@
 import {dirname} from "path";
 import {fileURLToPath} from "url";
 import {FlatCompat} from "@eslint/eslintrc";
+import reactCompilerPlugin from 'eslint-plugin-react-compiler';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...pluginQuery.configs['flat/recommended'],
+    {
+        name: 'react-compiler/recommended',
+        plugins: {
+            'react-compiler': reactCompilerPlugin,
+        },
+        rules: {
+            'react-compiler/react-compiler': 'error',
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
 ];
 
 export default eslintConfig;
